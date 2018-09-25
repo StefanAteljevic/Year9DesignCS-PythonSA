8762316cacf5dd77d7f348106dcbee9ff8c99a5a
Stefan Ateljevic <stefan.ateljevic@Y22-Design-Stefan-Ateljevic.local>
2018-09-25 09:14:59 -0400

    Input for Volume of Cylinder

--- VolumeOfCylinderGUI.py
+++ VolumeOfCylinderGUI.py
@@ -1,12 +1,18 @@
+import math
+
 print("This program calculates the volume of")
 print("a cylinder given radius and height")
 
 #Input
-r = input("What is the radius")
-h = input("What is the height")
+r = input("What is the radius") #When inputs are taken it is assumed they are strings
+r = float(r)                    #Casting r to a float and storing the result in r
+
+h = float(input("What is the height: "))
 
 #Process
+#Volume = PI*radius^2*height
 
+v = math.pi*r*r*h
 
 
 #Output
