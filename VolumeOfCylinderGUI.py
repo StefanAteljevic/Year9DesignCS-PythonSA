826d30198696daa30e8924ea937124399aacbeea
Stefan Ateljevic <stefan.ateljevic@Y22-Design-Stefan-Ateljevic.local>
2018-09-25 08:51:52 -0400

    Input for the volume of a cylinder

---
+++ VolumeOfCylinderGUI.py
@@ -0,0 +1,14 @@
+print("This program calculates the volume of")
+print("a cylinder given radius and height")
+
+#Input
+r = input("What is the radius")
+h = input("What is the height")
+
+#Process
+
+
+
+#Output
+
+print("END PROGRAM")
