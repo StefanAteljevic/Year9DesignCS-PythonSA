73d5c04ef3ba58795832ec75c69edaf4007d533e
Stefan Ateljevic <stefan.ateljevic@Y22-Design-Stefan-Ateljevic.local>
2018-11-13 19:15:16 -0500

    String example, alphabetical order

---
+++ If with strings (alphabetical order).py
@@ -0,0 +1,12 @@
+word1 = input("What is the first word:")
+word2 = input("What is the second word:")
+
+word1 = word1.upper()
+word2 = word2.upper()
+
+if (word1.upper == word2.upper):
+	print("The words are the same")
+elif: (word1.upper <word2.upper)
+	print("The first word is before the second alphabetically")
+else: 
+	print("The second word is before the first alphabetically")
